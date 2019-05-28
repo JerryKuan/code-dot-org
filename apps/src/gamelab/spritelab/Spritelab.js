174cab6266c96ad0076ef54b306552fe113a61cb
Anjali <anjali@code.org>
2019-05-28 16:58:46 -0700

    Sprite Commands
    createNewSprite, destroy, displace, getProp, makeNewSpriteAnon, setAnimation

--- apps/src/gamelab/spritelab/Spritelab.js
+++ apps/src/gamelab/spritelab/Spritelab.js
@@ -9,6 +9,31 @@ var Spritelab = function() {
     this.createEdgeSprites();
     this.drawSprites();
   };
+
+  // Sprite commands
+  this.commands.createNewSprite = function(name, animation, location) {
+    return commands.makeSprite.apply(this, [animation, location]);
+  };
+
+  this.commands.destroy = function(spriteId) {
+    commands.destroy(spriteId);
+  };
+
+  this.commands.displace = function(spriteId, targetSpriteIndex) {
+    commands.displace(spriteId, targetSpriteIndex);
+  };
+
+  this.commands.getProp = function(spriteId, prop) {
+    return commands.getProp(spriteId, prop);
+  };
+
+  this.commands.makeNewSpriteAnon = function(animation, location) {
+    commands.makeSprite.apply(this, [animation, location]);
+  };
+
+  this.commands.setAnimation = function(spriteId, animation) {
+    commands.setAnimation(spriteId, animation);
+  };
 };
 
 module.exports = Spritelab;
--- apps/src/gamelab/spritelab/spriteCommands.js
+++ apps/src/gamelab/spritelab/spriteCommands.js
@@ -1,3 +1,72 @@
 import * as spriteUtils from './spriteUtils';
 
-export const commands = {};
+export const commands = {
+  destroy(spriteId) {
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    sprites.forEach(sprite => {
+      sprite.destroy();
+      spriteUtils.deleteSprite(sprite.id);
+    });
+  },
+
+  displace(spriteId, targetSpriteIndex) {
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    let targetSprites = spriteUtils.singleOrGroup(targetSpriteIndex);
+    sprites.forEach(sprite => {
+      targetSprites.forEach(target => sprite.displace(target));
+    });
+  },
+
+  getProp(spriteId, prop) {
+    if (!spriteId) {
+      return undefined;
+    }
+    let sprite = spriteUtils.singleOrGroup(spriteId)[0];
+    if (prop === 'scale') {
+      return sprite.scale * 100;
+    } else if (prop === 'costume') {
+      return sprite.getAnimationLabel();
+    } else if (prop === 'y') {
+      return 400 - sprite.y;
+    } else {
+      return sprite[prop];
+    }
+  },
+
+  makeSprite(animation, location) {
+    if (!location) {
+      location = {x: 200, y: 200};
+    }
+    if (typeof location === 'function') {
+      location = location();
+    }
+    var sprite = this.createSprite(location.x, location.y);
+    sprite.direction = 0;
+    let spriteId = spriteUtils.addSprite(sprite);
+    if (animation) {
+      sprite.setAnimation(animation);
+      sprite.scale =
+        100 /
+        Math.max(
+          100,
+          sprite.animation.getHeight(),
+          sprite.animation.getWidth()
+        );
+    }
+    return spriteId;
+  },
+
+  setAnimation(spriteId, animation) {
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    sprites.forEach(sprite => {
+      sprite.setAnimation(animation);
+      sprite.scale =
+        100 /
+        Math.max(
+          100,
+          sprite.animation.getHeight(),
+          sprite.animation.getWidth()
+        );
+    });
+  }
+};
