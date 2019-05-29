bd8af0b2a4c05b1ff30ac9385d9f5c2fe834faf9
Anjali <anjali@code.org>
2019-05-29 10:45:33 -0700

    Action Commands
    - changePropBy, edgesDisplace, isTouchingEdges, jumpTo, mirrorSprite, moveInDirection, moveForward, moveToward, removeTint, setProp, setTint, turn

--- apps/src/gamelab/spritelab/actionCommands.js
+++ apps/src/gamelab/spritelab/actionCommands.js
@@ -1,3 +1,124 @@
 import * as spriteUtils from './spriteUtils';
 
-export const commands = {};
+export const commands = {
+  changePropBy(spriteId, prop, val) {
+    if (!val) {
+      return;
+    }
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    let specialCases = {
+      scale: sprite => (sprite.scale += val / 100),
+      y: sprite => (sprite.y -= val)
+    };
+    sprites.forEach(sprite => {
+      if (specialCases[prop]) {
+        specialCases[prop](sprite);
+      } else {
+        sprite[prop] += val;
+      }
+    });
+  },
+  edgesDisplace(spriteId) {
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    sprites.forEach(sprite => this.edges.displace(sprite));
+  },
+  isTouchingEdges(spriteId) {
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    let touching = false;
+    sprites.forEach(sprite => {
+      if (sprite.isTouching(this.edges)) {
+        touching = true;
+      }
+    });
+    return touching;
+  },
+  jumpTo(spriteId, location) {
+    if (!location) {
+      return;
+    }
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    sprites.forEach(sprite => {
+      sprite.x = location.x;
+      sprite.y = location.y;
+    });
+  },
+  mirrorSprite(spriteId, direction) {
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    sprites.forEach(sprite => {
+      if (direction === 'right') {
+        sprite.mirrorX(1);
+      } else {
+        sprite.mirrorX(-1);
+      }
+    });
+  },
+  moveForward(spriteId, distance) {
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    sprites.forEach(sprite => {
+      if (sprite.direction) {
+        let direction = sprite.direction % 360;
+        sprite.x += distance * Math.cos((direction * Math.PI) / 180);
+        sprite.y += distance * Math.sin((direction * Math.PI) / 180);
+      }
+    });
+  },
+  moveInDirection(spriteId, distance, direction) {
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    let dirs = {
+      North: sprite => (sprite.y -= distance),
+      East: sprite => (sprite.x += distance),
+      South: sprite => (sprite.y += distance),
+      West: sprite => (sprite.x -= distance)
+    };
+    if (!dirs[direction]) {
+      console.error('invalid direction: ' + direction);
+    }
+    sprites.forEach(sprite => {
+      dirs[direction](sprite);
+    });
+  },
+  moveToward(spriteId, distance, target) {
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    sprites.forEach(sprite => {
+      if (sprite && target) {
+        let angle = Math.atan2(target.y - sprite.y, target.x - sprite.x);
+        if (angle) {
+          let dy = Math.sin(angle) * distance;
+          let dx = Math.cos(angle) * distance;
+          sprite.x += dx;
+          sprite.y += dy;
+        }
+      }
+    });
+  },
+  setProp(spriteId, prop, val) {
+    if (!val) {
+      return;
+    }
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    let specialCases = {
+      scale: sprite => (sprite.scale = val / 100),
+      y: sprite => (sprite.y = 400 - val)
+    };
+    sprites.forEach(sprite => {
+      if (specialCases[prop]) {
+        specialCases[prop](sprite);
+      } else {
+        sprite[prop] = val;
+      }
+    });
+  },
+  turn(spriteId, n, direction) {
+    if (!n) {
+      return;
+    }
+    let sprites = spriteUtils.singleOrGroup(spriteId);
+    sprites.forEach(sprite => {
+      if (direction === 'right') {
+        sprite.rotation += n;
+      } else {
+        sprite.rotation -= n;
+      }
+    });
+  }
+};
